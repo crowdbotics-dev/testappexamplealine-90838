00485e6e4a175ea85e16cfbdd50b791c3379cc2d
Aline Tavares <aline@crowdbotics.com>
2023-06-12 13:39:04 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI4/index.js
+++ screens/ScreenAI4/index.js
@@ -1,4 +1,3 @@
-import { api_v1_login_create } from "../../store/testappexamplealineAPI/authTokens.slice.js";
 import { useDispatch } from "react-redux";
 import React, { useState } from "react";
 import { View, Text, TextInput, Pressable, Image, StyleSheet } from "react-native";
@@ -13,17 +12,10 @@ const LoginScreen = () => {
     }} style={styles.logo} />
       <TextInput style={styles.input} onChangeText={setEmail} value={email} placeholder="Email" keyboardType="email-address" autoCapitalize="none" />
       <TextInput style={styles.input} onChangeText={setPassword} value={password} placeholder="Password" secureTextEntry />
-      <Pressable style={styles.button} onPress={onSubmit}>
+      <Pressable style={styles.button} onPress={() => console.log("test")}>
         <Text style={styles.buttonText}>Login</Text>
       </Pressable>
     </View>;
-
-  const onSubmit = () => {
-    dispatch(api_v1_login_create({
-      username: email,
-      password
-    }));
-  };
 };
 
 const styles = StyleSheet.create({
