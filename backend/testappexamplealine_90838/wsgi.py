44d46852aca72564099a6a5c2da67f84d5675c4b
Crowdbotics <engineering@crowdbotics.com>
2023-06-12 16:22:32 +0000

    Initial commit.

---
+++ backend/testappexamplealine_90838/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for testappexamplealine_90838 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testappexamplealine_90838.settings')
+
+application = get_wsgi_application()
